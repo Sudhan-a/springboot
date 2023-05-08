24a73cc7395a10865d6a7b57ee281175ff8e92bd
Sudhan-a <sudhan2929@gmail.com>
2023-05-08 15:56:08 +0530

    loan

--- educationalLoanPortal/src/main/java/com/example/demo/Repository/AdminRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.AdminModel;
-
-public interface AdminRepo extends JpaRepository<AdminModel, Integer> {
-
-}
